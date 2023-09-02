48d828664eaed8ed82490a5c7ac870e6f81a97e1
Futar <Futarimiti@163.com>
2023-09-02 14:10:51 +0800

    add util functions

--- app/Util.hs
+++ app/Util.hs
@@ -1,6 +1,9 @@
-module Util (canonical, canon, (==?)) where
+module Util where
 
 import           Data.Char                  (isMark, toLower)
+import           Data.List                  (genericTake)
+import           Numeric.Natural            (Natural)
+import           Rando                      (shuffle)
 import           Unicode.Char.Normalization (DecomposeMode (..), decompose)
 
 canonical :: String -> String
@@ -11,3 +14,14 @@ canon = head . canonical . pure
 
 (==?) :: String -> String -> Bool
 (==?) x y = map toLower x == map toLower y
+
+pick :: Natural -> [a] -> IO [a]
+pick n xs = genericTake n <$> shuffle xs
+
+-- | 0-based list update.
+updateAt :: Natural -> (a -> a) -> [a] -> [a]
+updateAt 0 f (x:xs) = f x:xs
+updateAt n f (x:xs)
+  | n < 0 = x:xs
+  | otherwise = x : updateAt (n - 1) f xs
+updateAt _ _ xs = xs
