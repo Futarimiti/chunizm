e71346db9cc2a8e95527e55715b0e5a51ba1eba5
Futar <Futarimiti@163.com>
2023-09-04 09:53:08 +0800

    add util functions

--- app/Puzzle/Types.hs
+++ app/Puzzle/Types.hs
@@ -1,7 +1,12 @@
+{-# LANGUAGE RecordWildCards #-}
+
 module Puzzle.Types where
 
-import           Data.List.NonEmpty (NonEmpty)
-import           Data.Set           (Set)
+import           Config.Types         (Config (..))
+import           Data.Functor.Compose
+import           Data.List.NonEmpty
+import           Data.List.NonEmpty   as NE (cycle)
+import           Data.Set             (Set, empty)
 import           Player.Types
 
 type Solution = [Solution1]
@@ -19,4 +24,17 @@ type Puzzle = [Puzzle1]
 data Round = Round { puzzle :: Puzzle
                    , opened :: Set Char
                    , player :: Maybe (NonEmpty Player)
-                   }
+                   } deriving (Show, Eq)
+
+startup :: Config -> Puzzle -> Round
+startup c p = Round { puzzle = p
+                    , opened = empty
+                    , player = fmap NE.cycle . getCompose . fmap Player . Compose . nonEmpty $ players c
+                    }
+
+-- turn to next player
+next :: Round
+     -> Round
+next r@Round{..} = r{ player = do _ :| rest <- player
+                                  nonEmpty rest
+                    }
